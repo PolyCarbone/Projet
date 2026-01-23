f94c4432eb09b8ef76637f5c088c328a516ae5f9
Theophile Arnould <m.theophile.arnould@gmail.com>
2026-01-23 17:13:21 +0100

    center logo

--- app/auth/portal/page.tsx
+++ app/auth/portal/page.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     return (
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col gap-1 p-6 md:p-10">
-                <div className="flex justify-center gap-2 md:justify-start mt-20">
+                <div className="flex justify-center gap-2 mt-20">
                     <div className="flex flex-col items-center gap-1 font-medium">
                         <Logo className="size-18" width={60} height={60} radiusClass="rounded-full" />
                         <span className="text-lg text-gray-600" >PolyCarbone</span>
